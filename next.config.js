f9d7273cb4c9101c2c1d86064a259cf6510c36c4
12core1 <12533753+visnkmr@users.noreply.github.com>
2023-08-26 00:45:27 +0530

    fix individual review autosizing

--- next.config.js
+++ next.config.js
@@ -31,7 +31,8 @@ module.exports = withBundleAnalyzer({
   // basePath: '/home/roger/Downloads/github/webpage/out',
   // assetPrefix: 'https://visnkmr.github.io/homepagev2/',
   // assetPrefix: 'https://visnk.pages.dev/',
-  // assetPrefix: 'https://cdn.jsdelivr.net/gh/visnkmr/hv2static@main/',
+    // assetPrefix: 'https://cdn.jsdelivr.net/gh/visnkmr/hv2static@main/',
+  assetPrefix: 'https://cdn.jsdelivr.net/gh/visnkmr/visnkmr.github.io@main/',
   // amp:false,
   // basePath: 'https://cdn.jsdelivr.net/gh/visnkmr/hv2static@master/',
   // async rewrites(){
