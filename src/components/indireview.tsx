8bcc2e5fb8948305a3ecffb8ff5600153b65cef1
12core1 <12533753+visnkmr@users.noreply.github.com>
2025-07-05 14:44:48 +0530

    fixing customer testimonial list

--- src/components/indireview.tsx
+++ src/components/indireview.tsx
@@ -5,22 +5,27 @@ import StoreIcons from "./storeicons";
 
 export default function indireview(review:review){
         return ( 
-            <div 
-            // key={review.} 
-            className="min-h-[15rem] w-60 sm:flex shadow-indigo-500/50 shadow-[0_0_15px_rgba(0,0,0,0.2)] rounded-2xl col-span-1 mx-5 xl:mx-4 mb-8  p-4 " >             
-            {/* <div className="w-120 p-4">
-                <img
-                src={image}
-                className="w-120 "/>
-            </div> */}
-            <div className="text-center">
-            <h3 className="font-bold text-center m-4">{apab.get(review.appname)!}</h3>
-            <LineClamp className="text-center" lines={2} text={review.review}/>
-            {/* <h5 className="font-bold text-center m-4">{storename}</h5> */}
+            <div
+    // key={review.}
+    className="relative min-h-[15rem] w-60 sm:flex shadow-indigo-500/50 shadow-[0_0_15px_rgba(0,0,0,0.2)] rounded-2xl col-span-1 mx-5 xl:mx-4 mb-8  p-4 " >
 
-            {/* <img src={image} className="w-32"/> */}
-            <StoreIcons{...{storename:review.storename,w:0}}/>
+    <div className="text-center">
+        <h3 className="font-bold text-center m-4">{apab.get(review.appname)!}</h3>
+        <LineClamp className="text-center" lines={2} text={review.review}/>
+        {/* <h5 className="font-bold text-center m-4">{storename}</h5> */}
+
+        {/* Removed the original flex div here as it's no longer needed for positioning StoreIcons */}
+        {/* <div className="flex">
+            <div className="absolute bottom-0">
+                <StoreIcons {...{storename:review.storename,w:0}}/>
             </div>
-            </div>
+        </div> */}
+    </div>
+
+    {/* This div is now absolutely positioned relative to the main card */}
+    <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2">
+        <StoreIcons {...{storename:review.storename,w:0}}/>
+    </div>
+</div>
             );
 }
